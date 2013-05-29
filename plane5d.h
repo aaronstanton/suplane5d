50b92579ed475071c2b28956991ab591b98fe917
aaronstanton <aaron.stanton@gmail.com>
2013-05-29 15:21:41 -0600

    first commit

---
+++ plane5d.h
@@ -0,0 +1,158 @@
+/* Copyright (c) Signal Analysis and Imaging Group (SAIG), University of Alberta, 2013.*/
+/* All rights reserved.                       */
+/* plane5d  :  $Date: May 2013- Last version May    2013  */
+
+#include "su.h"
+#include "cwp.h"
+#include "fftw3.h"
+
+void plane5d(float **d,
+             int nt,int nh,
+             float *mx,float *my,float *hx,float *hy,
+             int nevent,float *amp,float *t0,
+             float *v_mx,float *v_my,float *v_hx,float *v_hy,
+             float *curve_mx,float *curve_my,float *curve_hx,float *curve_hy,
+             float dt,float fmin,float fmax,float f0);
+void ricker_wavelet(float *w, float f,float dt);
+
+void plane5d(float **d,
+             int nt,int nh,
+             float *mx,float *my,float *hx,float *hy,
+             int nevent,float *amp,float *t0,
+             float *v_mx,float *v_my,float *v_hx,float *v_hy,
+             float *curve_mx,float *curve_my,float *curve_hx,float *curve_hy,
+             float dt,float fmin,float fmax,float f0)
+{
+  complex shift;
+  complex czero;
+  int padfactor;
+  int ntfft;
+  int nw;
+  int ih,it,iw;
+  int ifmin,ifmax;
+  complex Complex_I;
+  complex **D;
+  float omega;
+  float* w;
+  complex* W;
+  float delay;
+  float shift_mx, shift_my, shift_hx, shift_hy;
+  float* in1;
+  complex* out1;
+  fftwf_plan p1;
+  complex* in2;
+  float* out2;
+  fftwf_plan p2;
+  int N;
+  int ievent;
+  
+  czero.r=0;czero.i=0;  
+  padfactor = 2;
+  ntfft = npfar(padfactor*nt);
+  nw = (int) ntfft/2 + 1; 
+  /* fprintf(stderr,"nw=%d\n",nw);
+  fprintf(stderr,"nh=%d\n",nh); */
+  D = ealloc2complex(ntfft,nh);
+  Complex_I.r = 0;
+  Complex_I.i = 1;
+
+  w = ealloc1float(ntfft);
+  for (it=0;it<ntfft;it++) w[it] = 0;
+  ricker_wavelet(w,f0,dt);
+  
+  W = ealloc1complex(nw);
+  
+  
+  /**********************************************************************************************/
+  N = ntfft;
+  in1 = ealloc1float(N);
+  out1 = ealloc1complex(nw);
+  p1 = fftwf_plan_dft_r2c_1d(N, in1, (fftwf_complex*)out1, FFTW_ESTIMATE);
+  for(it=0;it<ntfft;it++){
+    in1[it] = w[it];
+  }
+  fftwf_execute(p1); 
+  for(iw=0;iw<nw;iw++){
+    W[iw] = out1[iw]; 
+  }
+  /**********************************************************************************************/
+  delay = dt*(trunc((2.2/f0/dt))+1)/2;
+  
+  for (ih=0;ih<nh;ih++){ 
+    for (iw=0;iw<nw;iw++){ 
+      D[ih][iw] =  czero;
+    }
+  } 
+
+
+  if(fmin>0){ 
+    ifmin = (int) truncf(fmin*dt*ntfft);
+  }
+  else{
+    ifmin = 0;
+  }
+  if(fmax*dt*ntfft<nw){ 
+    ifmax = (int) truncf(fmax*dt*ntfft);
+  }
+  else{
+    ifmax = 0;
+  }
+
+  for (ievent=0;ievent<nevent;ievent++){ 
+    for (ih=0;ih<nh;ih++){ 
+      for (iw=ifmin;iw<ifmax;iw++){
+        omega = (float) 2*PI*iw/ntfft/dt;
+        shift_mx = powf(fabsf(mx[ih]),curve_mx[ievent])/v_mx[ievent];
+        shift_my = powf(fabsf(my[ih]),curve_my[ievent])/v_my[ievent];
+        shift_hx = powf(fabsf(hx[ih]),curve_hx[ievent])/v_hx[ievent];
+        shift_hy = powf(fabsf(hy[ih]),curve_hy[ievent])/v_hy[ievent];
+        /*
+        fprintf(stderr,"shift_mx=%f shift_my=%f shift_hx=%f shift_hy=%f\n",shift_mx,shift_my,shift_hx,shift_hy);
+		*/
+        shift.r =   cos(omega*(t0[ievent] + shift_mx + shift_my + shift_hx + shift_hy - delay));
+        shift.i =  -sin(omega*(t0[ievent] + shift_mx + shift_my + shift_hx + shift_hy - delay));
+        D[ih][iw] = cadd(D[ih][iw],crmul(cmul(W[iw],shift),amp[ievent]));
+      }
+    } 
+  }
+  /**********************************************************************************************/
+  N = ntfft;
+  in2 = ealloc1complex(N);
+  out2 = ealloc1float(N);
+  p2 = fftwf_plan_dft_c2r_1d(N, (fftwf_complex*)in2, out2, FFTW_ESTIMATE);
+  for (ih=0;ih<nh;ih++){
+    for(iw=0;iw<nw;iw++){
+      in2[iw] = D[ih][iw];
+    }
+    fftwf_execute(p2); 
+    for(it=0;it<nt;it++){
+      d[ih][it] = out2[it]; 
+    }
+  }
+  /**********************************************************************************************/
+  for (ih=0;ih<nh;ih++) for (it=0; it<nt; it++) d[ih][it]=d[ih][it]/ntfft;
+
+  free2complex(D);
+  fftwf_destroy_plan(p1);
+  fftwf_free(in1); fftwf_free(out1);
+  fftwf_free(in2); fftwf_free(out2);
+  
+  return; 
+}
+void ricker_wavelet(float *w, float f,float dt)
+{
+  int iw, nw, nc;
+  float alpha, beta;  
+  nw = (int) 2*trunc((float) (2.2/f/dt)/2) + 1;
+  nc = (int) trunc((float) nw/2);
+ 
+  for (iw=0;iw<nw-2;iw++){
+    alpha = (nc-iw+1)*f*dt*PI;
+  	beta = alpha*alpha;
+    w[iw] = (1-beta*2)*exp(-beta);
+  }
+
+  return;
+}
+
+
---
+++ suplane5d.c
@@ -0,0 +1,366 @@
+/* Copyright (c) Signal Analysis and Imaging Group (SAIG), University of Alberta, 2013.*/
+/* All rights reserved.                       */
+/* suplane5d  :  $Date:May     2013- Last version May 2013  */
+
+#include "su.h"
+#include "cwp.h"
+#include "segy.h"
+#include "header.h"
+#include <time.h>
+#include "plane5d.h"
+
+#ifndef MARK
+#define MARK fprintf(stderr,"%s @ %u\n",__FILE__,__LINE__);fflush(stderr);
+#endif
+
+/*********************** self documentation **********************/
+char *sdoc[] = {
+  " 	   							                                  ",
+  " SUPLANE5D  model linear or curved events in 4 spatial dimensions  ",
+  "                                                                   ",
+  "           User provides:                                          ",
+  "                                                                   ",
+  "           Other parameters:                                       ",
+  "           verbose=0; (=1 to show messages)                        ",
+  "           dt=0.004; (sample rate)                                 ",
+  "           tmax=1; (end time)                                      ",
+  "           fmin = 0; (min frequency)                               ",
+  "           fmax = 0.5/dt; (max frequency)                          ",
+  "           f0 = 30; (dominant frequency of ricker wavelet)         ",
+  "           nevent=1; (number of events)                            ",
+  "                               ",
+  "           To acheive shot/receiver geometry code:                    ",
+  "           sxmin=50;           ",
+  "           sxmax=50;           ",
+  "           symin=50;           ",
+  "           symax=50;           ",
+  "           gxmin=50;           ",
+  "           gxmax=50;           ",
+  "           gymin=1;            ",
+  "           gymax=100;          ",
+  "           dsx=1;              ",
+  "           dsy=1;              ",
+  "           dgx=1;              ",
+  "           dgy=1;              ",
+  "                               ", 
+  "           To acheive midpoint/offset geometry code:                  ", 
+  "           mxmin=0;            ",
+  "           mxmax=0;            ",
+  "           mymin=0;            ",
+  "           mymax=0;            ",
+  "           hxmin=0;            ",
+  "           hxmax=0;            ",
+  "           hymin=0;            ",
+  "           hymax=0;            ",
+  "           dmx=1;              ",
+  "           dmy=1;              ",
+  "           dhx=1;              ",
+  "           dhy=1;              ",
+  "                               ",
+  "                               ",
+  "           For these parameters separate multiple values using a comma: ",
+  "           amp = 1; (amplitude for each event)                          ",
+  "           t0 = 0.2; (zero offset time for each event )                 ",
+  "           v_mx = 10000000; (velocity for each event in mx direction)   ",
+  "           v_my = 10000000; (velocity for each event in my direction)   ",
+  "           v_hx = 5000; (velocity for each event in hx direction)       ",
+  "           v_hy = 5000; (velocity for each event in hy direction)       ",
+  "           curve_mx = 2; (power multiplied against the distance to achieve curvature in mx direction) ",
+  "           curve_my = 2; (power multiplied against the distance to achieve curvature in my direction) ",
+  "           curve_hx = 2; (power multiplied against the distance to achieve curvature in hx direction) ",
+  "           curve_hy = 2; (power multiplied against the distance to achieve curvature in hy direction)",
+  "                                                                   ",
+  "           Example coding:                                         ",
+  "           # makes a single 2D shot gather                         ",
+  "           suplane5d verbose=1  > d.su                             ",
+  "           suxwigb wclip=-1 bclip=1 key=gy < d.su &                ",
+ NULL};
+/* Credits:
+ * Aaron Stanton
+ * Trace header fields accessed:
+ * Last changes: May : 2013 
+ */
+/**************** end self doc ***********************************/
+
+segy tr;
+
+int main(int argc, char **argv)
+{
+  int verbose;
+  time_t start,finish;
+  double elapsed_time;
+  int it,ih;
+  int nt, ntr, nh; 
+  int ievent,nevent;    /* number of events */
+  float *amp=NULL;	    /* array of amplitudes for each event */
+  float *t0=NULL;	    /* array of time at zero offset for each event */
+  float *v_mx=NULL;	    /* array of dips in the mx direction for each event */
+  float *v_my=NULL;	    /* array of dips in the my direction for each event */
+  float *v_hx=NULL;	    /* array of dips in the hx direction for each event */
+  float *v_hy=NULL;	    /* array of dips in the hy direction for each event */
+  float *curve_mx=NULL;	/* array of curvatures in the mx direction for each event */
+  float *curve_my=NULL;	/* array of curvatures in the my direction for each event */
+  float *curve_hx=NULL;	/* array of curvatures in the hx direction for each event */
+  float *curve_hy=NULL;	/* array of curvatures in the hy direction for each event */
+  float dt,tmax;
+  float f0,fmin,fmax;
+  float **d;  
+  float *sx,*sy,*gx,*gy,*mx,*my,*hx,*hy,*h,*az; 
+  float sxmin,sxmax,dsx;
+  float symin,symax,dsy;
+  float gxmin,gxmax,dgx;
+  float gymin,gymax,dgy;
+  float mxmin,mxmax,dmx;
+  float mymin,mymax,dmy;
+  float hxmin,hxmax,dhx;
+  float hymin,hymax,dhy;
+  int isx,nsx;
+  int isy,nsy;
+  int igx,ngx;
+  int igy,ngy;
+  int imx,nmx;
+  int imy,nmy;
+  int ihx,nhx;
+  int ihy,nhy;
+  int mode;
+  
+  fprintf(stderr,"*******SUPLANE5D*********\n");
+  /* Initialize */
+  initargs(argc, argv);
+  requestdoc(0); /* stdin not used */
+
+  start=time(0);    
+  /* Get parameters */
+  if (!getparint("verbose", &verbose)) verbose=0;
+  if (!getparint("ntr",&ntr)){ 
+    ntr = 1000000;
+    if (verbose) fprintf(stderr,"warning: ntr paramater not set; using ntr=%d\n",ntr);
+  }
+  if (!getparfloat("dt", &dt)) dt=0.004;
+  if (!getparfloat("tmax", &tmax)) tmax=1;
+  nt = (int) (tmax/dt) + 1;
+  if (!getparfloat("fmin",&fmin)) fmin = 0;
+  if (!getparfloat("fmax",&fmax)) fmax = 0.5/dt;
+  fmax = MIN(fmax,0.5/dt);
+  if (!getparfloat("f0",&f0)) f0 = 30; /* 30 Hz dominant freq for ricker wavelet */
+  if (!getparint("nevent",&nevent)){ 
+    nevent=1; 
+    amp = ealloc1float(nevent); amp[0] = 1;
+    t0 = ealloc1float(nevent); t0[0] = 0.2;
+    v_mx = ealloc1float(nevent); v_mx[0] = 10000000;
+    v_my = ealloc1float(nevent); v_my[0] = 10000000;
+    v_hx = ealloc1float(nevent); v_hx[0] = 5000;
+    v_hy = ealloc1float(nevent); v_hy[0] = 5000;
+    curve_mx = ealloc1float(nevent); curve_mx[0] = 2;
+    curve_my = ealloc1float(nevent); curve_my[0] = 2;
+    curve_hx = ealloc1float(nevent); curve_hx[0] = 2;
+    curve_hy = ealloc1float(nevent); curve_hy[0] = 2;
+  }
+  else{
+    if (!(nevent == countparval("amp"))) err("must give amp= vector");
+    amp = ealloc1float(nevent);	getparfloat("amp", amp);
+    
+    if (!(nevent == countparval("t0"))) err("must give t0= vector");
+    t0 = ealloc1float(nevent);	getparfloat("t0", t0);
+
+    if (!(nevent == countparval("v_mx"))) err("must give v_mx= vector");
+    if (!(nevent == countparval("v_my"))) err("must give v_my= vector");
+    if (!(nevent == countparval("v_hx"))) err("must give v_hx= vector");
+    if (!(nevent == countparval("v_hy"))) err("must give v_hy= vector");
+    v_mx = ealloc1float(nevent);	getparfloat("v_mx", v_mx);
+    v_my = ealloc1float(nevent);	getparfloat("v_my", v_my);
+    v_hx = ealloc1float(nevent);	getparfloat("v_hx", v_hx);
+    v_hy = ealloc1float(nevent);	getparfloat("v_hy", v_hy);
+    
+    if (!(nevent == countparval("curve_mx"))) err("must give curve_mx= vector");
+    if (!(nevent == countparval("curve_my"))) err("must give curve_my= vector");
+    if (!(nevent == countparval("curve_hx"))) err("must give curve_hx= vector");
+    if (!(nevent == countparval("curve_hy"))) err("must give curve_hy= vector");
+    curve_mx = ealloc1float(nevent);	getparfloat("curve_mx", curve_mx);
+    curve_my = ealloc1float(nevent);	getparfloat("curve_mx", curve_mx);
+    curve_hx = ealloc1float(nevent);	getparfloat("curve_hx", curve_hx);
+    curve_hy = ealloc1float(nevent);	getparfloat("curve_hy", curve_hy);
+  }
+  if (verbose){
+    for (ievent=0;ievent<nevent;ievent++){ 
+      fprintf(stderr,"amp[%d]      =%f\n",ievent,amp[ievent]);
+      fprintf(stderr,"t0[%d]       =%f\n",ievent,t0[ievent]);
+      fprintf(stderr,"v_mx[%d]   =%f\n",ievent,v_mx[ievent]);
+      fprintf(stderr,"v_my[%d]   =%f\n",ievent,v_my[ievent]);
+      fprintf(stderr,"v_hx[%d]   =%f\n",ievent,v_hx[ievent]);
+      fprintf(stderr,"v_hy[%d]   =%f\n",ievent,v_hy[ievent]);
+      fprintf(stderr,"curve_mx[%d] =%f\n",ievent,curve_mx[ievent]);
+      fprintf(stderr,"curve_my[%d] =%f\n",ievent,curve_my[ievent]);
+      fprintf(stderr,"curve_hx[%d] =%f\n",ievent,curve_hx[ievent]);
+      fprintf(stderr,"curve_hy[%d] =%f\n",ievent,curve_hy[ievent]);
+    } 
+  }
+
+
+  if (!getparfloat("sxmin", &sxmin)) sxmin=50;
+  if (!getparfloat("sxmax", &sxmax)) sxmax=50;
+  if (!getparfloat("symin", &symin)) symin=50;
+  if (!getparfloat("symax", &symax)) symax=50;
+  if (!getparfloat("gxmin", &gxmin)) gxmin=50;
+  if (!getparfloat("gxmax", &gxmax)) gxmax=50;
+  if (!getparfloat("gymin", &gymin)) gymin=1;
+  if (!getparfloat("gymax", &gymax)) gymax=100;
+  if (!getparfloat("dsx", &dsx)) dsx=1;
+  if (!getparfloat("dsy", &dsy)) dsy=1;
+  if (!getparfloat("dgx", &dgx)) dgx=1;
+  if (!getparfloat("dgy", &dgy)) dgy=1;
+  /*  */
+  if (!getparfloat("mxmax", &mxmax)) mxmax=0;
+  if (!getparfloat("mymin", &mymin)) mymin=0;
+  if (!getparfloat("mymax", &mymax)) mymax=0;
+  if (!getparfloat("hxmin", &hxmin)) hxmin=0;
+  if (!getparfloat("hxmax", &hxmax)) hxmax=0;
+  if (!getparfloat("hymin", &hymin)) hymin=0;
+  if (!getparfloat("hymax", &hymax)) hymax=0;
+  if (!getparfloat("dmx", &dmx)) dmx=1;
+  if (!getparfloat("dmy", &dmy)) dmy=1;
+  if (!getparfloat("dhx", &dhx)) dhx=1;
+  if (!getparfloat("dhy", &dhy)) dhy=1;
+  
+  mode = 1;
+  nsx = (int) truncf((sxmax - sxmin)/dsx) + 1;
+  nsy = (int) truncf((symax - symin)/dsy) + 1;
+  ngx = (int) truncf((gxmax - gxmin)/dgx) + 1;
+  ngy = (int) truncf((gymax - gymin)/dgy) + 1;
+  nmx = (int) truncf((mxmax - mxmin)/dmx) + 1;
+  nmy = (int) truncf((mymax - mymin)/dmy) + 1;
+  nhx = (int) truncf((hxmax - hxmin)/dhx) + 1;
+  nhy = (int) truncf((hymax - hymin)/dhy) + 1;
+  if (!getparfloat("mxmin",&mxmin)){
+  	mode = 1;
+    mxmin = 0;
+    if (verbose) fprintf(stderr,"using surface coordinates (sx,sy,gx,gy)\n");
+  }
+  else{
+  	mode = 2;
+    if (verbose) fprintf(stderr,"using subsurface coordinates (mx,my,hx,hy)\n");
+  }
+
+  if (mode==1) nh = nsx*nsy*ngx*ngy;
+  else         nh = nmx*nmy*nhx*nhy;
+  /* Allocate memory for data */
+  /* fprintf(stderr,"nt=%d\n",nt);
+  fprintf(stderr,"nh=%d\n",nh); */
+
+  d  = ealloc2float(nt,nh);
+  sx = ealloc1float(nh);
+  sy = ealloc1float(nh);
+  gx = ealloc1float(nh);
+  gy = ealloc1float(nh);
+  mx = ealloc1float(nh);
+  my = ealloc1float(nh);
+  hx = ealloc1float(nh);
+  hy = ealloc1float(nh);
+  h  = ealloc1float(nh);
+  
+  if (verbose) fprintf(stderr,"creating %d traces \n", nh);
+
+  for (ih=0;ih<nh;ih++){ 
+    for (it=0;it<nt;it++){   
+      d[ih][it] = 0;
+    }
+  }
+  if (mode==1){ 
+    ih = 0;   
+    for (isx=0;isx<nsx;isx++){ 
+      for (isy=0;isy<nsy;isy++){ 
+        for (igx=0;igx<ngx;igx++){ 
+          for (igy=0;igy<ngy;igy++){ 
+            sx[ih] = isx*dsx + sxmin;
+            sy[ih] = isy*dsy + symin; 
+            gx[ih] = igx*dgx + gxmin; 
+            gy[ih] = igy*dgy + gymin; 
+            ih++;
+          }
+        }
+      }
+    } 
+  } 
+  else{ 
+    ih = 0;   
+    for (imx=0;imx<nmx;imx++){ 
+      for (imy=0;imy<nmy;imy++){ 
+        for (ihx=0;ihx<nhx;ihx++){ 
+          for (ihy=0;ihy<nhy;ihy++){ 
+            mx[ih] = imx*dmx + mxmin;
+            my[ih] = imy*dmy + mymin; 
+            hx[ih] = ihx*dhx + hxmin; 
+            hy[ih] = ihy*dhy + hymin; 
+            ih++;
+          }
+        }
+      }
+    } 
+  } 
+  if (mode==1){ 
+    for (ih=0;ih<nh;ih++){
+      mx[ih] = (gx[ih] + sx[ih])/2;
+      my[ih] = (gy[ih] + sy[ih])/2;
+      hx[ih] = gx[ih] - sx[ih];
+      hy[ih] = gy[ih] - sy[ih];      
+      h[ih]  = sqrt(hx[ih]*hx[ih] + hy[ih]*hy[ih]);      
+    } 
+  } 
+  else{ 
+    for (ih=0;ih<nh;ih++){
+      sx[ih] = mx[ih] - 0.5*hx[ih];
+      sy[ih] = my[ih] - 0.5*hy[ih];
+      gx[ih] = sx[ih] + hx[ih];
+      gy[ih] = sy[ih] + hy[ih];
+      h[ih]  = sqrt(hx[ih]*hx[ih] + hy[ih]*hy[ih]);      
+    } 
+  } 
+  
+  
+  plane5d(d,
+          nt,nh,
+          mx,my,hx,hy,
+          nevent,amp,t0,
+          v_mx,v_my,v_hx,v_hy,
+          curve_mx,curve_my,curve_hx,curve_hy,
+          dt,fmin,fmax,f0);
+  
+    
+ /* ***********************************************************************
+ outputting data:
+ *********************************************************************** */
+  for (ih=0;ih<nh;ih++){ 
+    memcpy((void *) tr.data,(const void *) d[ih],nt*sizeof(float));
+    tr.ntr = nh;
+    tr.sx  = (int) sx[ih];
+    tr.sy  = (int) sy[ih];
+    tr.gx  = (int) gx[ih];
+    tr.gy  = (int) gy[ih];
+    tr.gelev  = (int) mx[ih];
+    tr.selev  = (int) my[ih];
+    tr.gdel   = (int) hx[ih];
+    tr.sdel   = (int) hy[ih];
+    tr.offset = (int) h[ih];
+    tr.ns = nt;
+    tr.dt = NINT(dt*1000000.);
+    tr.tracl = tr.tracr = ih + 1;
+    fputtr(stdout,&tr);
+  }
+
+
+ /* ***********************************************************************
+ end outputting data
+ *********************************************************************** */
+ free2float(d);
+
+ /******** End of output **********/
+ finish=time(0);
+ elapsed_time=difftime(finish,start);
+ fprintf(stderr,"Total time required: %6.2f \n", elapsed_time);
+ 
+ return EXIT_SUCCESS;
+}
+
+
+
+
